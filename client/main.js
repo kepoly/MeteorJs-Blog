410677d4f83b87a29aca749594dbc84c852eba70
Matthew Spada <spadamatthew@gmail.com>
2016-04-22 02:09:27 -0400

    Header footer addition

--- client/main.js
+++ client/main.js
@@ -2,6 +2,12 @@ import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
 import '../imports/ui/user/user'
 import '../imports/ui/loading.html';
+import '../imports/ui/headfoot.html';
+
+
+Router.configure({
+    layoutTemplate: 'headfoot'
+});
 
 Router.route('/',function() {
     this.render('home'), {
