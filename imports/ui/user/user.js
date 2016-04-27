34e0d6ac78c6f71ef8409c0bf50a07adabe1fa58
Matthew Spada <spadamatthew@gmail.com>
2016-04-26 20:34:44 -0400

    Small little update to user page. Comments are only shown on individual posts.

--- imports/ui/user/user.js
+++ imports/ui/user/user.js
@@ -7,6 +7,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Posts } from '../../api/posts.js';
+import { Comments } from '../../api/comments.js';
 
 import './user.html';
 
@@ -21,9 +22,37 @@ if(Meteor.isServer) {
     });
 }
 
+Template.registerHelper('FormatDate', function(date){
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+});
+
 Template.user.onCreated(function userOnCreated() {
     this.state = new ReactiveDict();
     Meteor.subscribe('posts');
+    Meteor.subscribe('comments');
 });
 
 Template.user.helpers({
@@ -32,8 +61,15 @@ Template.user.helpers({
     },
     userposts() {
         return Posts.find({ username: { $in: [Template.instance().data] } }, { sort: { createdAt: -1 }  });
+    },
+    extendedData() {
+        var returnDetails = {};
+        var postCount = Posts.find({username: {$in: [Template.instance().data]}}).count();
+        var commentCount = Comments.find({username: {$in: [Template.instance().data]}}).count();
+        returnDetails["postCount"] = postCount;
+        returnDetails["commentCount"] = commentCount;
+        return returnDetails;
     }
-
 });
 
 Template.user.events({
